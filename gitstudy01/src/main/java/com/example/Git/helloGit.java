c590a170603eed14f286393758cd556ec350a62e
魏振华 <cherishWZH@163.com>
2020-08-27 23:54:05 +0800

    创建分支

--- gitstudy01/src/main/java/com/example/Git/helloGit.java
+++ gitstudy01/src/main/java/com/example/Git/helloGit.java
@@ -4,5 +4,6 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("版本一");
+        System.out.println("创建分支");
     }
 }
