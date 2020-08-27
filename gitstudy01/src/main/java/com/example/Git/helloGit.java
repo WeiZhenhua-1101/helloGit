2e03acf05f28bcdab7d9559e4e956f88be0fcb32
魏振华 <cherishWZH@163.com>
2020-08-27 23:25:51 +0800

    版本1

--- gitstudy01/src/main/java/com/example/Git/helloGit.java
+++ gitstudy01/src/main/java/com/example/Git/helloGit.java
@@ -3,5 +3,6 @@ package com.example.Git;
 public class helloGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("版本一");
     }
 }
